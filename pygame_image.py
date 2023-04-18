d42fea176a0b1ccb8e5cf78523e94ef5da350f53
haruki <c0b22136c1@edu.teu.ac.jp>
2023-04-18 17:06:59 +0900

    演習課題

--- pygame_image.py
+++ pygame_image.py
@@ -9,28 +9,32 @@ def main():
     kk_img=pg.image.load("ex01/fig/3.png")
     kk_img=pg.transform.flip(kk_img,True,False)
     kk_imgs=[kk_img,pg.transform.rotozoom(kk_img,10,1.0)]
+    
+    
 
     tmr = 0
     x=0
-    
+    t=0
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         tmr += 1
+        if tmr%20==0:
+           t+=1
         x=tmr%1600
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img,[1600-x,0])
+        screen.blit(pg.transform.flip(bg_img,True,False),[1600-x,0])
 
     
 
-        screen.blit(kk_imgs [0],[300,200])
+        screen.blit(kk_imgs [t%2],[300,200])
         
         
 
         pg.display.update()
-        clock.tick(100)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
